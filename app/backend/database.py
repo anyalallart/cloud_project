1d1eb862eec7611ab43405de75e43dcc82c21fd1
Ethan <ethan.marfjan@student.junia.com>
2025-12-08 23:45:11 +0100

    fix: DB path variable name fixed

--- app/backend/database.py
+++ app/backend/database.py
@@ -11,7 +11,7 @@ load_dotenv("environment.env")
 
 DB_USERNAME = os.getenv("DB_USERNAME")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_PATH")
+DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME", "testpixelo")
 
