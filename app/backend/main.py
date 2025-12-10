aa7caaa1d2a99542946efeac1e453b7744cec52f
Ethan <ethan.marfjan@student.junia.com>
2025-12-10 23:15:59 +0100

    fix: change allowed connexion to all

--- app/backend/main.py
+++ app/backend/main.py
@@ -4,21 +4,21 @@ from pydantic import BaseModel
 from sqlalchemy.orm import Session
 import models
 from database import SessionLocal, engine
+import os
+from dotenv import load_dotenv
 
+load_dotenv("environment.env")
+
+FRONTEND_URL = os.getenv("FRONTEND_URL")
 # Create the database tables
 app = FastAPI()
 
 # enable CORS so browsers can perform OPTIONS preflight requests
-origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,      
+    allow_origins=["*"],      
     allow_credentials=True,
     allow_methods=["*"],        
     allow_headers=["*"],        
